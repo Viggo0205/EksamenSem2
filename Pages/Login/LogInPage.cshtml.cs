04cd8feaddba94099178817ca86910ae7a09c43b
Auden12 <137095274+Auden12@users.noreply.github.com>
2024-05-08 10:54:58 +0200

    Visual studio fucking crashed og har kun gemt de sider nej har lavet >:(

---
+++ Pages/Login/LogInPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EksamenSem2.Pages.Login
+{
+    public class LogInPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
