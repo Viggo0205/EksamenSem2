05ee81a2a357e4908ca469286fdb5bd560704901
Matti Barlach <mattibarlach@gmail.com>
2024-05-15 07:28:45 +0200

    k

--- Services/Interfaces/IDataService.cs
+++ Services/Interfaces/IDataService.cs
@@ -6,7 +6,7 @@ public interface IDataService<T> where T : class, IHasId
 
     T? Read(int id);
 
-    bool Delete(int id); // T? bool Delete(int id); - ???, ville hjælpe til delete af medarbejder i FjernMedarbejder.cshtml.cs
+    bool Delete(int id);
 
     List<T> GetAll();
 }
