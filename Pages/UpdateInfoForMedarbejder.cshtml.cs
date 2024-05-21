d5b256cc74482dd354a57e46558c6a2deb58316e
Victor Tadeusz Ulstrup Olszowski <Victor.olszowski@gmail.com>
2024-05-21 10:55:18 +0200

    Update til medarbejder

---
+++ Pages/UpdateInfoForMedarbejder.cshtml.cs
@@ -0,0 +1,40 @@
+using EksamenSem2.Models;
+using EksamenSem2.Pages.Login;
+using EksamenSem2.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EksamenSem2.Pages
+{
+    public class UpdateInfoForMedarbejderModel : PageModel
+    {
+
+        [BindProperty]
+        public string Navn { get; set; }
+        [BindProperty]
+        public string Password {  get; set; }
+
+        private IMedabejderDataService _medarbejderDataService;
+
+
+        public UpdateInfoForMedarbejderModel(IMedabejderDataService medabejderDataService)
+        {
+                _medarbejderDataService= medabejderDataService;
+        }
+        public void OnGet()
+        {
+            Medarbejder m = _medarbejderDataService.Read(LogInPageModel.LoggedInMedarbejder.Id);
+            Navn = m.Navn;
+            Password = m.Password;
+
+
+        }
+
+        public IActionResult OnPost()
+        {
+            _medarbejderDataService.UpdateInfoForMedarbejder(LogInPageModel.LoggedInMedarbejder.Id, Navn, Password);
+            return RedirectToPage("/Index");
+        }
+
+    }
+}
