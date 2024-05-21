685393fa2efdb31b9e3fbcbdf51271acb4ed62ec
jonasosa <93657845+jonasosa@users.noreply.github.com>
2024-05-21 12:33:47 +0200

    overabejde + kanlender(næsten)

--- Services/Interfaces/IVagtPlanDataService.cs
+++ Services/Interfaces/IVagtPlanDataService.cs
@@ -7,5 +7,8 @@ namespace EksamenSem2.Services.Interfaces
     {
         List<PlanDatum> GetPlanDataWithIncludes();
         void AddPlanData(PlanDatum planData);
+        VagtPlan GetById(int id);
+        void SaveChanges();
+        VagtPlan RegisterOverTime(VagtPlan vagtPlan, double time, string description);
     }
 }
